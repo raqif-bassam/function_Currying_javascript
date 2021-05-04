f03f3d3aab7b9b0ac69f37749f863ca992b8210c
Md. Raqif Bassam <raqif.bassam@wunderman.com>
2021-05-04 17:32:08 +0600

    added example from MDN for function currying

--- JavaScript_Function_Curring/functionCurrying.js
+++ JavaScript_Function_Curring/functionCurrying.js
@@ -23,3 +23,20 @@ multiply2ByTwo(6);
 
 let multiply2yByFive = multiply2(5);
 multiply2yByFive(7);
+
+//Another example from MDN
+function curry(f) { // curry(f) does the currying transform
+  return function(a) {
+    return function(b) {
+      return f(a, b);
+    };
+  };
+}
+
+// usage
+function sum(a, b) {
+  return a + b;
+}
+
+let curriedSum = curry(sum);
+console.log(carriedSum);
